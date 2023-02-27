71f07243041d14a79b77490ae8f4a14476330ae9
isaias04 <pchapeltique@gmail,com>
2023-02-26 19:49:49 -0600

    portafolio

--- src/main.js
+++ src/main.js
@@ -2,7 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
 import VueSmoothScroll from 'vue3-smooth-scroll'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+
 
 const app = createApp(App)
 app.use(VueSmoothScroll)
 app.mount('#app')
+
